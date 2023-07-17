863bbc9aa231361562db2952a646d0fb7b110af4
Vuklorr <vova.krypenichka@yandex.ru>
2023-07-17 16:40:00 +0300

    @fix add the task text

--- java/src/main/java/com/algos/binsearch/Task5.java
+++ java/src/main/java/com/algos/binsearch/Task5.java
@@ -1,5 +1,17 @@
 package com.algos.binsearch;
 
+/**
+ * Sqrt(x).
+ * Given a non-negative integer x, return the square root of x rounded down to the nearest integer.
+ * The returned integer should be non-negative as well.
+ *
+ * You must not use any built-in exponent function or operator.
+ * For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ *
+ * Constraints:
+ * 0 <= x <= 231 - 1
+ */
+
 public class Task5 {
     public int mySqrt(int x) {
         if(x == 1) {
