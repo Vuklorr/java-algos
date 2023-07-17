1360421a15ec9ac1425ed280b19f16366fa3f624
Vuklorr <vova.krypenichka@yandex.ru>
2023-07-17 13:07:36 +0300

    @fix fixed bin search

--- java/src/main/java/com/algos/binsearch/Task2.java
+++ java/src/main/java/com/algos/binsearch/Task2.java
@@ -24,28 +24,28 @@ package com.algos.binsearch;
 
 public class Task2 {
     public int search(int[] nums, int target) {
+        if(nums.length == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+
+
         int left = 0;
         int right = nums.length - 1;
 
-        while(left <= right) {
+        while(left < right) {
             int mid = left + (right - left) / 2;
 
-            if(nums[mid] == target) {
+            if (nums[mid] == target) {
                 return mid;
-            } else if(nums[left] <= nums[mid]) {
-                if(target >= nums[left] && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+            }
+
+            if ((nums[left] < nums[mid] && (target < nums[mid] && target >= nums[left])) ||
+                    (nums[left] > nums[mid] && (target < nums[mid] || target >= nums[left]))) {
+                right = mid;
             } else {
-                if (target > nums[mid] && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+                left = mid + 1;
             }
         }
-        return -1;
+        return nums[left] == target ? left : -1;
     }
 }
